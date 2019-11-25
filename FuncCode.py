f946723251bbea0c71418427cef50416e9af90eb
ellinj2 <ellinj2@rpi.edu>
2019-11-25 14:57:03 -0500

    Update to work with Xavier's Instruction class

--- FuncCode.py
+++ FuncCode.py
@@ -3,31 +3,45 @@ RegData={"$s0":10, "$s1":15, "$s2":20}
 
 class Instruction:
     def __init__(self, opin, r1,r2,r3):
-        self.op=opin
-        self.rd=r1
-        self.rs=r2
-        self.rt=r3
+        self.operation=opin
+        self.RD=r1
+        self.RS=r2
+        self.RT=r3
 
 def ALU(instr):
     if type(instr) is not Instruction:
         raise Exception ("The variable being passed into the ALU is not an instruction!")
 
-    if instr.op=="add" or instr.op=="addi":
-        RegData[instr.rd]=int(RegData[instr.rs])+int(RegData[instr.rt])
-    elif instr.op=="and" or instr.op=="andi":
-        RegData[instr.rd]=int(RegData[instr.rs])&int(RegData[instr.rt])
-    elif instr.op=="or" or instr.op=="ori":
-        RegData[instr.rd]=int(RegData[instr.rs])|int(RegData[instr.rt])
-    elif instr.op=="slt" or instr.op=="slti":
-        RegData[instr.rd]=int(int(RegData[instr.rs])<int(RegData[instr.rt]))
-    elif instr.op=="beq":
-        RegData[instr.rd]=int(int(RegData[instr.rs])==int(RegData[instr.rt]))
-    elif instr.op=="bne":
-        RegData[instr.rd]=int(int(RegData[instr.rs])!=int(RegData[isntr.rt]))
+    first=0
+    second=0
+    if instr.RS=="$zero":
+        first=0
+    elif instr.RS not in RegData:
+        first=int(instr.RS)
+    else:
+        first=int(RegData[instr.RS])
+    if instr.RT=="$zero":
+        second=0
+    elif instr.RT not in RegData:
+        second=int(instr.RT)
+    else:
+        second=int(RegData[instr.RT])
+    if instr.operation=="add" or instr.operation=="addi":
+        RegData[instr.RD]=first+second
+    elif instr.operation=="and" or instr.operation=="andi":
+        RegData[instr.RD]=first&second
+    elif instr.operation=="or" or instr.operation=="ori":
+        RegData[instr.RD]=first|second
+    elif instr.operation=="slt" or instr.operation=="slti":
+        RegData[instr.RD]=int(first<second)
+    elif instr.operation=="beq":
+        RegData[instr.RD]=int(first==second)
+    elif instr.operation=="bne":
+        RegData[instr.RD]=int(first!=second)
 
     else:
         raise Exception ("The operation is not supported by the ALU")
 
 
-ALU(Instruction("sub", "$s0", "$s1", "$s2"))
+ALU(Instruction("addi", "$s0", "$s1", "$s2"))
 print(RegData["$s0"])
