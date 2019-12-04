17504a706ecfbe2f5de25bee574c92cd05b170f1
ellinj2 <ellinj2@rpi.edu>
2019-12-04 12:49:06 -0500

    Updated for syntax and clarity

--- Control.py
+++ Control.py
@@ -14,7 +14,16 @@ class Control(object):
         self.forwardB=False
         self.BranchDestination=None
 
+    '''
+    Forward keys:
+    0: Inactive
+    1: Execution forward
+    2: Memory forward
+    '''
     def checkDataHazards(self):
+        self.DataHazardFlag=False
+        self.forwardA=0
+        self.forwardB=0
         IFID=0
         IDEX=1
         EXMEM=2
@@ -22,24 +31,24 @@ class Control(object):
         EXHazard=False
         if pipeline_registers[EXMEM].input.RD==pipeline_registers[IDEX].input.RS:
             self.DataHazardFlag=True
-            self.ForwardA=10
+            self.ForwardA=1
             EXHazard=True
         if pipeline_registers[EXMEM].input.RD==pipeline_registers[IDEX].input.RT:
             self.DataHazardFlag=True
-            self.ForwardB=10
+            self.ForwardB=1
             EXHazard=True
         if (not EXHazard) and (pipeline_registers[MEMWB].input.RD==pipeline_registers[IDEX].input.RS):
             self.DataHazardFlag=True
-            self.ForwardA=01
+            self.ForwardA=2
         if (not EXHazard) and (pipeline_registers[MEMWB].input.RD==pipeline_registers[IDEX].input.RT):
             self.DataHazrdFlag=True
-            self.ForwardB=01
+            self.ForwardB=2
 
     def BranchValue():
         instr=pipeline_registers[MEMWB].ouput
-        if instr.operation=="beq" && instr.RS==instr.RT:
+        if instr.operation=="beq" and instr.RS==instr.RT:
             return labelDictionary[isntr.RD]
-        if instr.operation=="bne" && instr.RS!=instr.RT:
+        if instr.operation=="bne" and instr.RS!=instr.RT:
             return labelDictionary[instr.RD]
         return -1
 
