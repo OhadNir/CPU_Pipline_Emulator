b008120d2d97562fe1c231e217909741b0565f49
xmarshall39 <48485277+xmarshall39@users.noreply.github.com>
2019-12-06 10:17:25 -0500

    Integration into main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from FuncCode import ALU
+from instruction import Instruction
 global register_data, pipeline_registers, pipeline_history, all_instructions, cycle_count, max_cycle_count, next_instruction_index
 register_data = {
         "$s0": 0,
@@ -20,6 +21,7 @@ register_data = {
         "$t8": 0,
         "$t9": 0
     } 
+branch_labels = dict()
 pipeline_registers = list()
 pipeline_history = list()
 all_instructions = list()
@@ -32,27 +34,50 @@ def read_file():
     #reads input file
     return "inst"
 
-def make_pipeline():
-    #reads input file
-    input_file = read_file()
-    for instruction in input_file:
-        #convert insuction strings into instruction class objects.
-        #then
-        all_instructions.append(instruction)
-
+def make_pipeline(filename):
+    next_index = 1
+    
+    file = open(filename, 'r', encoding = "utf-8")
+    for line in file:
+        #HIT A LABEL (All labels end in ':')
+        if line.strip()[-1] == ':': 
+            branch_labels[line.strip()] = next_index
+            continue
+        
+        all_instructions.append(Instruction(line.strip()))
+        next_index += 1
+    
+    file.close()
     return
 
 def make_pipereg():
     return
 
-def print_pipeline():
-    return
-
+#I'm assuming this is the function to print the entire pipeline
 def print_register():
+    print("CPU Cycles ===>\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\t13\t14\t15\t16")
+    for instr in all_instructions:
+        print(instr)
+    
+    print("")
+    i = 0
+    for reg in register_data.keys():
+        if i == len(register_data.keys()) - 1:
+            print(reg + " = " + str(register_data[reg]))
+        elif i < 3:
+            print(reg + " = " + str(register_data[reg]) + "\t", sep = "", end = "")
+        else:
+            print(reg + " = " + str(register_data[reg]), sep = "")
+
+    print("-" * 82)
     return
     
 if __name__ == '__main__':
 
+    isForwarding = False
+    print("START OF SIMULATION (" + "no" if isForwarding else "" + "forwarding)" )
+    print("-" * 82)
+    
     for cycle_count in range(len(max_cycle_count)):
         temp = control.CheckBranch()
         if temp != -1: 
